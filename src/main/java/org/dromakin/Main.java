91131ac60704a7c57731caa2cc3f72c87e5c1d17
Dmitriy Romakin <32188375+dromakin@users.noreply.github.com>
2023-04-29 20:12:28 +0300

    add homework * task 2

--- src/main/java/org/dromakin/Main.java
+++ src/main/java/org/dromakin/Main.java
@@ -1,55 +1,59 @@
 package org.dromakin;
 
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
 
         long startTs = System.currentTimeMillis(); // start time
-        List<Thread> threads = new ArrayList<>();
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        List<Future<Integer>> futureTasks = new ArrayList<>();
 
         for (String text : texts) {
-            Thread thread = new Thread(
-                    () -> {
-                        int maxSize = 0;
-                        for (int i = 0; i < text.length(); i++) {
-                            for (int j = 0; j < text.length(); j++) {
-                                if (i >= j) {
-                                    continue;
-                                }
-                                boolean bFound = false;
-                                for (int k = i; k < j; k++) {
-                                    if (text.charAt(k) == 'b') {
-                                        bFound = true;
-                                        break;
-                                    }
-                                }
-                                if (!bFound && maxSize < j - i) {
-                                    maxSize = j - i;
-                                }
+            Callable<Integer> task = () -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
                             }
                         }
-                        System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
                     }
-            );
+                }
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                return maxSize;
+            };
 
-            thread.start();
-            threads.add(thread);
-
+            Future<Integer> taskResult = executor.submit(task);
+            futureTasks.add(taskResult);
         }
 
-        for (Thread thread : threads) {
-            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+        int max = 0;
+        for (Future<Integer> task : futureTasks) {
+            max = Math.max(max, task.get());
         }
+
+        executor.shutdown();
 
         long endTs = System.currentTimeMillis(); // end time
-
         System.out.println("Time: " + (endTs - startTs) + "ms");
+        System.out.println("Max len: " + max);
     }
 
     public static String generateText(String letters, int length) {
